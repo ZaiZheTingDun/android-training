ba33cfee40358a5baef3174666f672f49f603edf
xis <aragaki9527@gmail.com>
2021-07-08 22:32:59 +0800

    [Activity] Pick Contact

--- app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
@@ -3,11 +3,14 @@ package com.thoughtworks.androidtrain
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     companion object {
+        private const val REQUEST_SELECT_CONTACT = 1
         private const val TAG = "MainActivity"
     }
 
@@ -27,6 +30,45 @@ class MainActivity : AppCompatActivity() {
         loginButton.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
+        }
+
+        val pickContactButton = findViewById<Button>(R.id.button3)
+        pickContactButton.setOnClickListener {
+            selectContact()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_SELECT_CONTACT && resultCode == RESULT_OK && data != null) {
+            val contactUri = data.data
+            if (contactUri != null) {
+                val projection = arrayOf(
+                    ContactsContract.Data._ID,
+                    ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                    ContactsContract.CommonDataKinds.Phone.NUMBER
+                )
+                val query = contentResolver.query(contactUri, projection, null, null, null)
+                query?.use {
+                    it.moveToNext()
+
+                    val displayName = it.getString(it.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
+                    val phoneNumber = it.getString(it.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+
+                    Toast.makeText(this, "$displayName $phoneNumber", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun selectContact() {
+        val intent = Intent(Intent.ACTION_PICK).apply {
+            type = ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE
+            addCategory(Intent.CATEGORY_DEFAULT)
+        }
+        Log.d(TAG, "test")
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivityForResult(intent, REQUEST_SELECT_CONTACT)
         }
     }
 }
